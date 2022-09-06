28d487420a5ccbc573d419fe8af116556dfda00d
Kevin Huynh <kvhuynh820@gmail.com>
2022-09-05 17:24:57 -0700

    initial todolist completed

--- algos/script.js
+++ algos/script.js
@@ -1,4 +1,1 @@
-let name = "kevin"
-for (let i = 0; i < name.length; i ++) {
-    console.log(name[5]);
-}
+
--- react/todo-list/src/App.js
+++ react/todo-list/src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+
+
+import React, { useState } from "react";
 
 function App() {
+  // input box
+  const [newTodo, setNewTodo] = useState("");
+
+  const [currentListTodos, setCurrentListTodos] = useState([]);
+
+  const handleNewTodo = (e) => {
+    e.preventDefault() // always prevent default when submmiting form
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      isComplete: false
+    }
+
+    setCurrentListTodos([...currentListTodos, todoItem]);
+    setNewTodo("");
+  }
+
+  const handleDeleteItem = (e, index) => {
+    const filteredTodos = currentListTodos.filter((todo, i) => {
+      return i !== index
+    });
+
+    setCurrentListTodos(filteredTodos)
+  }
+
+  const handleCompletion = (index) => {
+    const updatedTodos = currentListTodos.map((todo, i) => {
+      if (index === i) {
+        // todo.complete = !todo.complete;
+        const alteredTodo = {...todo, isComplete: !todo.isComplete};
+        return alteredTodo
+      }
+      return todo;
+    });
+
+    setCurrentListTodos(updatedTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => {
+        handleNewTodo(e);
+      }}>
+        <input onChange={(e) => { setNewTodo(e.target.value)}} type="text" value={newTodo} />
+        <br />
+        <button>add</button>
+      </form>
+
+      {currentListTodos.map((todo, i) => {
+        const todoClasses =["bold"];
+
+        if (todo.isComplete) {
+          todoClasses.push("line-through")
+        }
+
+        return (
+          <div key={i}>
+            <span className={todoClasses.join(" ")}>{todo.text}</span>
+            <input onChange={(e) => {handleCompletion(i)}} checked={todo.isComplete} type="checkbox" />
+            <button onClick={(e) => {handleDeleteItem(e, i)}}>delete</button>
+          </div>)})}
+
+
     </div>
   );
 }
