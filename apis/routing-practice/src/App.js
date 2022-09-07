daadead4ec1e6f3b7e35efc14c2bfc506c37da96
Kevin Huynh <kvhuynh820@gmail.com>
2022-09-06 20:28:04 -0700

    completed routing-practice assignment

--- apis/routing-practice/src/App.js
+++ apis/routing-practice/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import {Routes, Route, Link, useParams} from "react-router-dom";
+
+const Home = (props) => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  )
+}
+
+const Input = (props) => {
+  const { number } = useParams();
+
+  
+  return (
+    <div>
+      <h1>{ number }</h1>
+    </div>
+  )
+}
+
+const MultiColorWord = (props) => {
+  const { word, textColor, backgroundColor } = useParams();
+  console.log(word, textColor, backgroundColor);
+
+  return (
+  <div>
+    <h1 style={{color: textColor, backgroundColor: backgroundColor}}>{word}</h1>
+  </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/:number" element={<Input />}></Route>
+        <Route path="/:word/:textColor/:backgroundColor" element={<MultiColorWord />}></Route>
+      </Routes>
     </div>
   );
 }
