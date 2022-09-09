ac5a07ffb4e7cba97ddd32ac1957042928fe4187
Kevin Huynh <kvhuynh820@gmail.com>
2022-09-08 21:09:05 -0700

    added height and width features

--- react/box-generator/src/components/UserForm.js
+++ react/box-generator/src/components/UserForm.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 export const UserForm = (props) => {
     const [colorList, setColorList] = useState([]);
     const [newColor, setNewColor] = useState("");
-    const [newSize, setNewSize] = useState("");
+    const [height, setHeight] = useState("");
+    const [width, setWidth] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -11,23 +12,35 @@ export const UserForm = (props) => {
         if (newColor.length === 0) {
             return;
         }
-
-        setColorList([...colorList, newColor]);
+        const boxInfo = [newColor, height + "px", width + "px"];
+        setColorList([...colorList, boxInfo])
         setNewColor("");
 
     }
 
-    const handleChange = (e) => {
-        setNewColor(e.target.value);
-        console.log(e.target.value);
-        console.log(colorList);
+    const handleColorChange = (e) => {
+        const color = e.target.value;
+        setNewColor(color);
+    }
+
+    const handleHeightChange = (e) => {
+        const height = e.target.value;
+        setHeight(height);
+    }
+
+    const handleWidthChange = (e) => {
+        const width = e.target.value;
+        setWidth(width);
     }
 
     return(
         <div className="container">
             <h1>Color</h1>
             <form onSubmit={ (e) => handleSubmit(e) }>
-                <input type="text" name="color" value={newColor} onChange={ (e) => handleChange(e) }/>
+                <input type="text" name="color" value={newColor} onChange={ (e) => handleColorChange(e) }/>
+                <input type="number" name="color" value={height} onChange={ (e) => handleHeightChange(e) }/>
+                <input type="number" name="color" value={width} onChange={ (e) => handleWidthChange(e) }/>
+
                 <button>Add</button>
             </form>
 
@@ -36,7 +49,7 @@ export const UserForm = (props) => {
                 {colorList.map((color) => {
                     console.log(color);
                     return(
-                        <section className="box" style={{backgroundColor: color}}></section>
+                        <section className="box" style={{backgroundColor: color[0], height: color[1], width: color[2]}}></section>
                     )
                 })}
             </main>
@@ -45,6 +58,5 @@ export const UserForm = (props) => {
 
     )
 }
-
 
 export default UserForm;
