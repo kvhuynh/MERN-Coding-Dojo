c425f5477b7853ffb66877a1b7ec02f64549e7a7
Kevin Huynh <kvhuynh820@gmail.com>
2022-09-08 17:00:33 -0700

    finished assignments

--- react/box-generator/src/components/UserForm.js
+++ react/box-generator/src/components/UserForm.js
@@ -2,28 +2,39 @@ import { useState } from "react";
 
 export const UserForm = (props) => {
     const [colorList, setColorList] = useState([]);
+    const [newColor, setNewColor] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (newColor.length === 0) {
+            return;
+        }
+
+        setColorList([...colorList, newColor]);
+
     }
 
     const handleChange = (e) => {
-        setColorList(e.target.value);
+        setNewColor(e.target.value);
+        console.log(e.target.value);
+        console.log(colorList);
     }
 
     return(
         <div className="container">
             <h1>Color</h1>
-            <form onSubmit={ handleSubmit }>
-                <input type="text" name="color" value={ color }onChange={ handleChange }/>
-                <button type="submit">Add</button>
+            <form onSubmit={ (e) => handleSubmit(e) }>
+                <input type="text" name="color" onChange={ (e) => handleChange(e) }/>
+                <button>Add</button>
             </form>
 
 
-            <main className="flex-row flex-wrap">
-                {colorList.map((color, i) => {
+            <main className="flex flex-wrap">
+                {colorList.map((color) => {
+                    console.log(color);
                     return(
-                        <section className="box" style={{backgroundColor: color[i]}}></section>
+                        <section className="box" style={{backgroundColor: color}}></section>
                     )
                 })}
             </main>
